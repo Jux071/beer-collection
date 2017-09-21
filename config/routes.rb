80aaa2038b1ed6c4b62fc7701c073a33bbdc8cad
Jux071 <jux071@gmail.com>
2017-09-21 14:43:47 +0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :styles
   resources :breweries
 
-  resources :users, only: [:new, :create, :show]
+  #resources :users, only: [:new, :create, :show]
 	resource :session, only: [:new, :create, :destroy]
 
 end
